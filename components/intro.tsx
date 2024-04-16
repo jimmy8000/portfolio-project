25e173163bb992949b852fd4ffaefc1c159c5ffa
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2024-04-16 17:15:04 -0500

    Refactor download link and fix typo in intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -75,7 +75,7 @@ export default function Intro() {
         >
           Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a href="/cv.pdf" download="cv.pdf" className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/5" href="/CV.pdf" download>
+        <a href="/CV.pdf" download className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/5" href="/CV.pdf" download>
           Download resume <HiDownload  className="opacity-60 group-hover:translate-y-1 transition"/>
         </a>
         <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5"
